5949a281abf4725942456bb29ee626ec86d33e81
Superwheat <94495972+Superwheat@users.noreply.github.com>
2024-12-29 11:46:04 +0100

    Update antiafk.lua

--- antiafk.lua
+++ antiafk.lua
@@ -1,17 +1,12 @@
 local vu = cloneref(game:GetService("VirtualUser"))
 local plr = game:GetService("Players").LocalPlayer
+
 plr.Idled:connect(function()
 	vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 	task.wait(1)
 	vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
 
-plr.OnTeleport:Connect(function(State)
-	if State == Enum.TeleportState.Started then
-		loadstring(game:HttpGet("https://pastebin.com/raw/ReqaZ3Hs"))()
-	end
-end)
-
 queue_on_teleport([[
-	
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/Superwheat/s-x/refs/heads/main/antiafk.lua"))()
 ]])
